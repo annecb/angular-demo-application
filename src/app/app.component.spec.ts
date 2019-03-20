9ad17b2090ee9f3c82234aa225f4f2eb60ccdf0b
Anne-Charlotte Bournigal <anne-charlotte.bournigal@moskitos.fr>
2019-03-20 16:09:19 +0100

    Fix Unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,22 +2,25 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { WeatherService } from './shared/services/weather.service'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MaterialModule } from './shared/material.module'
+import { WeatherCardComponent } from './shared/components/weather-card/weather-card.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        WeatherCardComponent
       ],
       providers: [WeatherService],
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule, MaterialModule]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
+    fixture.debugElement.nativeElement.style.visibility = "hidden";
     expect(app).toBeTruthy();
   });
-
 });
--- src/app/shared/components/weather-card/weather-card.component.spec.ts
+++ src/app/shared/components/weather-card/weather-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WeatherCardComponent } from './weather-card.component';
+import { MaterialModule } from './../../material.module'
 
 describe('WeatherCardComponent', () => {
   let component: WeatherCardComponent;
@@ -8,14 +9,53 @@ describe('WeatherCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WeatherCardComponent ]
+      declarations: [WeatherCardComponent],
+      imports: [MaterialModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WeatherCardComponent);
     component = fixture.componentInstance;
+    component.weatherData = {
+      current: {
+        last_updated_epoch: 1552567517,
+        last_updated: "2019-03-14 13:45",
+        temp_c: 10.0,
+        temp_f: 50.0,
+        is_day: 1,
+        condition: {
+          text: "Light rain",
+          icon: "//cdn.apixu.com/weather/64x64/day/296.png",
+          code: 1183
+        },
+        wind_mph: 21.7,
+        wind_kph: 34.9,
+        wind_degree: 250,
+        wind_dir: "WSW",
+        pressure_mb: 1010.0,
+        pressure_in: 30.3,
+        precip_mm: 1.9,
+        precip_in: 0.07,
+        humidity: 87,
+        cloud: 75,
+        feelslike_c: 6.3,
+        feelslike_f: 43.3,
+        uv: 3.0
+      },
+      location: {
+        name: "Paris",
+        region: "Ile-de-France",
+        country: "France",
+        lat: 48.87,
+        lon: 2.33,
+        tz_id: "Europe/Paris",
+        localtime_epoch: 1552568642,
+        localtime: "2019-03-14 14:04"
+      }
+    };
+
     fixture.detectChanges();
   });
 
